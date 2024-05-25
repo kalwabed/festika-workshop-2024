d344917cbfd1d76f36de322e259fdc54f23c2417
kalwabed <49640654+kalwabed@users.noreply.github.com>
2024-05-25 21:28:52 +0700

    feat: password verification

--- backend/src/modules/authentication/controller.js
+++ backend/src/modules/authentication/controller.js
@@ -1,4 +1,6 @@
 import jwt from "jsonwebtoken";
+import argon2 from "@node-rs/argon2";
+import db from "../../lib/database.js";
 
 export async function signIn(req, res) {
   const { username, password } = req.body;
@@ -9,10 +11,62 @@ export async function signIn(req, res) {
       .json({ success: false, message: "Username or password is empty!" });
   }
 
-  const token = jwt.sign({ username }, "very-secret-KEY", {
+  await db.read();
+  const user = db.data.users.find((user) => user.username === username);
+
+  if (!user) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Username or password is not valid!" });
+  }
+
+  const isPasswordValid = await argon2.verify(user.password, password);
+
+  if (!isPasswordValid) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Username or password is not valid!" });
+  }
+
+  const token = jwt.sign({ username, id: user.id }, "very-secret-KEY", {
     algorithm: "HS512",
     expiresIn: "1 day",
   });
 
   return res.json({ success: true, token });
+}
+
+export async function signUp(req, res) {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Username or password is empty!" });
+  }
+
+  await db.read();
+  const user = db.data.users.find(
+    (user) => user.username === username.toLowerCase(),
+  );
+
+  if (user) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Username is already taken" });
+  }
+
+  const encryptedPassword = await argon2.hash(password);
+
+  db.data.users.push({
+    id: crypto.randomUUID(),
+    username: username.toLowerCase(),
+    password: encryptedPassword,
+  });
+  await db.write();
+
+  return res.json({
+    success: true,
+    message: "User has been registered.",
+  });
 }
--- backend/src/modules/authentication/route.js
+++ backend/src/modules/authentication/route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signIn } from "./controller.js";
+import { signIn, signUp } from "./controller.js";
 
 const router = express.Router();
 
 router.post("/login", signIn);
+router.post("/signup", signUp);
 
 export default router;
